ac42a8419617390a84d9a44013111b1f2b518ad9
Olga Butolina <solsarok@gmail.com>
2020-09-04 10:21:51 +0200

    Task №3. Using Rate in Review

--- src/components/review.js
+++ src/components/review.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Rate from './rate';
 
 function Review(props) {
   return (
     <div>
       <h4>{props.review.user}</h4>
-      <p>Review: {props.review.rating}</p>
+      <Rate rate={props.review.rating} />
       <p>{props.review.text}</p>
     </div>
   );
