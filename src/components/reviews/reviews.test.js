c4ed9eaaf53429b8d6f761736d71c746366ec1c9
Olga Butolina <solsarok@gmail.com>
2020-09-07 17:50:18 +0200

    Task 3. Add reviews.test

---
+++ src/components/reviews/reviews.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Reviews from './reviews';
+import { restaurants } from '../../fixtures';
+
+const reviews = restaurants[0].reviews;
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Reviews', () => {
+  it('should render', () => {
+    const wrapper = mount(<Reviews reviews={reviews} />);
+    expect(wrapper.find('.reviews').length).toBe(1);
+  });
+});
